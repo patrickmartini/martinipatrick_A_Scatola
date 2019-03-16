bb1cadfbe7e161bd06d449b3c867dc066aaadc64
Patrick Martini <patrick.martini@barsanti.edu.it>
2019-03-16 09:13:26 +0100

    2a modifica

--- martinipatrick_Scatola/martinipatrick_Scatola.ino
+++ martinipatrick_Scatola/martinipatrick_Scatola.ino
@@ -1,22 +1,56 @@
-int led = 6;
-int bottone = 7;
-int statobottone = 0;
+#include <LiquidCrystal.h>
+
+
+int led = 7;
+int ledRgb = 8;
+int btn = 9;
+int btn_ = 10;
+int btn1 = 11;
+int bzr = 6;
+int value = 0;
+LiquidCrystal lcd();
+
 void setup() {
-  pinMode(led, OUTPUT);
-  pinMode(bottone, INPUT);
+  randomSeed(millis);
+  pinMode(led,OUTPUT);
+  pinMode(ledRgb,OUTPUT);
+  pinMode(btn,INPUT);
+  pinMode(bzr,OUTPUT);
+
 }
+
 void loop() {
-  statobottone = digitalRead(bottone);
-  if (statobottone == HIGH)
-  {
-   digitalWrite(led, HIGH);
-   delay(5000);
-   digitalWrite(led, LOW);
-  }
-  else
-  {
-   digitalWrite(led, LOW);
-  }
-    
+  controllaBottone(int btn, int value);
+
 }
-  
+
+void controllaBottone(int bottone, int value){
+  if (value == HIGH){
+    iniziaTest();
+  }
+}
+
+void iniziaTest(int tempo){
+  tempo = random(1,5000);
+  delay(tempo);
+  digitalWrite(led,HIGH);
+  int ris = contatempo(btn_,0);
+  digitalWrite(led,LOW);
+  lcd.print(ris + "ms");
+  delay(3000);
+  lcd.clear();
+  tempo = random(3000,5000);
+  delay(tempo);
+  digitalWrite(bzr,HIGH);
+  int ris_
+}
+
+
+int contatempo(int value, int cont){
+  while(value == LOW)
+  {
+    cont++;
+    delay(1);
+  }
+  return value;
+}
