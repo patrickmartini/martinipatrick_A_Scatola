8a9a61cd58ed2adc01bf999bdd09bebdfad52113
Patrick Martini <patrick.martini@barsanti.edu.it>
2019-03-30 09:55:09 +0100

    3a Prova
    Funzionante ma da finire

--- martinipatrick_Scatola/martinipatrick_Scatola.ino
+++ martinipatrick_Scatola/martinipatrick_Scatola.ino
@@ -1,56 +1,84 @@
 #include <LiquidCrystal.h>
 
-
-int led = 7;
-int ledRgb = 8;
-int btn = 9;
-int btn_ = 10;
-int btn1 = 11;
-int bzr = 6;
+int led = 4;
+int ledRgbV = 7;
+int ledRgbR = 1;
+int btnInizio = 2;
+int btnLed = 3;
+int btnBuzzer = 5;
+int buzzer = 6;
 int value = 0;
-LiquidCrystal lcd();
+
+LiquidCrystal lcd(12,13,8,9,10,11);
 
 void setup() {
+  digitalWrite(buzzer,HIGH);
   randomSeed(millis);
   pinMode(led,OUTPUT);
-  pinMode(ledRgb,OUTPUT);
-  pinMode(btn,INPUT);
-  pinMode(bzr,OUTPUT);
+  pinMode(ledRgbV,OUTPUT);
+  pinMode(ledRgbR,OUTPUT);
+  pinMode(btnInizio,INPUT);
+  pinMode(btnLed,INPUT);
+  pinMode(btnBuzzer,INPUT);
+  pinMode(buzzer,OUTPUT);
+  lcd.begin(16,2);
 
 }
 
 void loop() {
-  controllaBottone(int btn, int value);
-
+  controllaBottone(digitalRead(btnInizio));
 }
 
-void controllaBottone(int bottone, int value){
+int controllaBottone(int value){
   if (value == HIGH){
-    iniziaTest();
+    eseguiTest(0);
   }
 }
-
-void iniziaTest(int tempo){
+void eseguiTest(int tempo){
+  reset();
+  lcd.print("Inizio Test");
+  delay(1000);
+  lcd.clear();
   tempo = random(1,5000);
   delay(tempo);
   digitalWrite(led,HIGH);
-  int ris = contatempo(btn_,0);
+  int ris = contatempo(btnLed,0);
   digitalWrite(led,LOW);
-  lcd.print(ris + "ms");
-  delay(3000);
+  String s = "ms";
+  lcd.print(ris + s);
+  delay(1000);
   lcd.clear();
-  tempo = random(3000,5000);
+  tempo = random(1,5000);
   delay(tempo);
-  digitalWrite(bzr,HIGH);
-  int ris_
+  digitalWrite(buzzer,LOW);
+  int ris_ = contatempo(btnBuzzer,0);
+  digitalWrite(buzzer,HIGH);
+  lcd.print(ris_ + s);
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(ris + s);
+  lcd.setCursor(0,1);
+  lcd.print(ris_ + s);
+  if (ris<400 && ris_<400){
+    analogWrite(ledRgbV,255);
+  }else{
+    analogWrite(ledRgbR,255);
+  }
 }
 
 
-int contatempo(int value, int cont){
-  while(value == LOW)
+int contatempo(int button, int cont){
+  while(digitalRead(button) == LOW)
   {
     cont++;
     delay(1);
   }
-  return value;
+  return cont;
+}
+
+void reset(){
+  analogWrite(ledRgbV,0);
+  analogWrite(ledRgbR,0);
+  lcd.clear();
 }
